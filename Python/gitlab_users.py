2eaa827d6cb4978825783c7886c6c5c1fca815f1
MDNacke <Mark.Nacke@gmail.com>
2018-09-22 15:39:58 -0400

    Added Python code

---
+++ Python/gitlab_users.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+########################################################################################
+# Copyright (c) 2016, Cummins Inc. Business System. All rights reserved.
+########################################################################################
+
+import os
+import sys
+import json
+import subprocess
+import signal
+import pycurl
+import re
+import csv
+from subprocess import Popen, PIPE
+
+from io import BytesIO
+import StringIO
+
+headers = {}
+myFile = open('/tmp/gitlab_users.csv', 'w')
+record = [['WWID', 'Name', 'Email']]
+
+def header_function(header_line):
+    # HTTP standard specifies that headers are encoded in iso-8859-1.
+    # On Python 2, decoding step can be skipped.
+    # On Python 3, decoding step is required.
+    header_line = header_line.decode('iso-8859-1')
+
+    # Header lines include the first status line (HTTP/1.x ...).
+    # We are going to ignore all lines that don't have a colon in them.
+    # This will botch headers that are split on multiple lines...
+    if ':' not in header_line:
+        return
+
+    # Break the header line into header name and value.
+    name, value = header_line.split(':', 1)
+
+    # Remove whitespace that may be present.
+    # Header lines include the trailing newline, and there may be whitespace
+    # around the colon.
+    name = name.strip()
+    value = value.strip()
+
+    # Header names are case insensitive.
+    # Lowercase name here.
+    name = name.lower()
+
+    headers[name] = value
+
+## curl --head -H "Private-Token: 6_p-J248zCysgBBQNjHP" https://gitlab.cummins.com/api/v4/users
+URL = 'https://gitlab.cummins.com/api/v4/users'
+Headers = 'Private-Token: 6_p-J248zCysgBBQNjHP'
+
+# Setup curl values for GitLab API
+buffer = BytesIO()
+ch = pycurl.Curl()
+ch.setopt(ch.HEADER, True)
+ch.setopt(ch.URL, URL)
+ch.setopt(ch.HTTPHEADER, [Headers])
+ch.setopt(ch.NOBODY, True)
+ch.setopt(ch.HEADERFUNCTION, header_function)
+ch.setopt(ch.WRITEFUNCTION, lambda x: None)
+
+ch.perform()
+ch.close()
+
+Total_Users = headers['x-total']
+Num_pages = int(Total_Users)//100
+remainder = int(Total_Users)%100
+
+print ('Total users = ' + str(Total_Users) )
+if (remainder > 0 ):
+    Num_pages += 2
+
+print ('Total pages needed = ' + str(Num_pages) )
+
+writer = csv.writer(myFile)
+writer.writerows(record)
+for x in range(1, Num_pages):
+## curl -H "Private-Token: 6_p-J248zCysgBBQNjHP" https://gitlab.cummins.com/api/v4/users?per_page=100&page=(page number)
+    URL = 'https://gitlab.cummins.com/api/v4/users?per_page=100&page=' + str(x)
+    Headers = 'Private-Token: 6_p-J248zCysgBBQNjHP'
+
+    pages = pycurl.Curl()
+    data = BytesIO()
+    pages.setopt(pages.URL, URL)
+    pages.setopt(pages.HTTPHEADER, [Headers])
+    pages.setopt(pages.WRITEFUNCTION, data.write)
+
+    pages.perform()
+    pages.close()
+
+    Results = data.getvalue()
+    dict = json.loads(Results)
+
+    for id in dict:
+        getName = str(id["name"])
+        getUserid  = str(id["username"])
+        getEmail = str(id["email"])
+        record[0][0] = getUserid
+        record[0][1] = getName
+        record[0][2] = getEmail
+        writer.writerows(record)
+
+myFile.close()
+
+#Command = 'echo "GitLab user report in CSV format" | mutt -s "Gitlab monthly user reporting" -a "/tmp/gitlab_users.csv" -- kd657@cummins.com cb698@cummins.com ka507@cummins.com 2> /tmp/email.log'
+Command = 'echo "GitLab production user report in CSV format" | mutt -s "Production Gitlab monthly user reporting" -a "/tmp/gitlab_users.csv" -- ka507@cummins.com 2> /tmp/email.log'
+outPut = Popen( Command , shell=True, stderr=PIPE, stdout=PIPE)
+
+print ( outPut.communicate()[0] )
