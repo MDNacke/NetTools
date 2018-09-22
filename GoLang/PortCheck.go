2472acfc8ef72b56dac00802107880e238bc18cf
MDNacke <Mark.Nacke@gmail.com>
2018-09-22 15:08:23 -0400

    Corrected PortCheck code

--- GoLang/PortCheck.go
+++ GoLang/PortCheck.go
@@ -1,89 +1,51 @@
 package main
 
 import (
-        "os"
-        "fmt"
-        "net"
-        "os/exec"
-        "strings"
-        "github.com/fatih/color"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
 )
 
-func Hosts(cidr string) ([]string, error) {
-        var numips int = 0
-        var startip int = 0
-        bits := strings.Split(cidr, "/")
-        if len(bits) < 2 {
-             fmt.Println("Invalid CIDR Notation, ", cidr)
-             os.Exit(1)
-        }
-
-        requestedbits := bits[1]
-        if (requestedbits == "31") {
-           fmt.Println("CIDR format not valid, cannot use 31 bits")
-           os.Exit(2)
-        }
-
-        ip, ipnet, err := net.ParseCIDR(cidr)
-        if err != nil {
-                return nil, err
-        }
-
-        var ips []string
-
-        for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-                ips = append(ips, ip.String())
-        }
-
-        // remove network address and broadcast address
-
-        numips = len(ips)
-        if (requestedbits == "32") {
-             numips = 1
-        } else {
-             numips--
-             startip = 1
-        }
-        return ips[startip : numips], nil
-}
-
-
-func inc(ip net.IP) {
-        for j := len(ip) - 1; j >= 0; j-- {
-                ip[j]++
-                if ip[j] > 0 {
-                        break
-                }
-        }
-}
-
+// Run the port scanner
 func main() {
-        mystring := " "
-        if len(os.Args) < 2 {
-            fmt.Print("No CIDR supplied \n")
-            os.Exit(1)
-        }
-        CIDR := os.Args[1]
-        fmt.Println(" ")
-        mystring = "Pinging addresses for, "
-        mystring += CIDR
-        color.Yellow(mystring)
-        hosts, _  := Hosts(CIDR)
-        lastip := len(hosts)
-        lastip--
-        fmt.Println("First IP =", hosts[0])
-        fmt.Println("Last IP  =", hosts[lastip])
-        fmt.Println(" ")
+    var host, port string
+    flag.StringVar(&host, "H", "", "host to scan")
+    flag.StringVar(&port, "p", "", "port to scan")
+    flag.Parse()
 
-        for i := 0; i < len(hosts); i++ {
-            _, err := exec.Command("ping", "-c1", "-t3", "-w2", hosts[i]).Output()
-            mystring = hosts[i]
-            if err != nil {
-                mystring += ", offline"
-                color.Red(mystring)
-            } else {
-                mystring += ", online"
-                color.Green(mystring)
+    if host == "" || port == "" {
+        fmt.Println("Usage: portscan -H <host> -p port")
+        os.Exit(1)
+    }
+
+    d := net.Dialer{Timeout: 20*time.Second}
+    conn, errors := d.Dial("tcp", fmt.Sprintf("%v:%v", host, port))
+		fmt.Println(" ")
+		fmt.Println("Running check for host", host, "on port", port)
+		fmt.Println(" ")
+
+    if errors != nil {
+        if oerr, ok := errors.(*net.OpError); ok {
+            switch oerr.Err.(type) {
+                case *os.SyscallError:
+                    fmt.Println("connect: connection refused to", host, "on port", port )
+                default:
+                    if oerr.Timeout() {
+                        fmt.Println("connect: connection timed out to", host, "on port", port )
+                    } else {
+                        panic("Unknown connection error")
+                    }
             }
         }
+    } else {
+    fmt.Println("connect: connection successful to", host, "on port", port )
+    }
+
+    if conn != nil {
+        conn.Close()
+    }
+		fmt.Println(" ")
+		
 }
