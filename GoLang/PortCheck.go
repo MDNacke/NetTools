45c07fef14cf96285e678ad1fafbc1bf15c99b1e
MDNacke <Mark.Nacke@gmail.com>
2018-09-23 12:42:13 -0400

    Updated color scheme

--- GoLang/PortCheck.go
+++ GoLang/PortCheck.go
@@ -7,11 +7,13 @@ import (
 	"os"
 	"time"
 	"strings"
+	"github.com/fatih/color"
 )
 
 // Run the port scanner
 func main() {
     var host, port string
+		var PrintString string
     flag.StringVar(&host, "H", "", "host to scan")
     flag.StringVar(&port, "p", "", "port to scan")
     flag.Parse()
@@ -21,35 +23,43 @@ func main() {
         os.Exit(1)
     }
 
+    fmt.Println(" ")
+    now := time.Now()
+    startnow := now.Format("Mon Jan _2 15:04:05 2006")
+
+    PrintString = fmt.Sprintf("%v - Running PortCheck for host %v on port %v", startnow, host, port)
+    color.Green(PrintString)
+    fmt.Println(" ")
     d := net.Dialer{Timeout: 20*time.Second}
     conn, errors := d.Dial("tcp", fmt.Sprintf("%v:%v", host, port))
-    fmt.Println(" ")
-    fmt.Println("Running check for host", host, "on port", port)
-    fmt.Println(" ")
-
+    now = time.Now()
+    endnow := now.Format("Mon Jan _2 15:04:05 2006")
     if errors != nil {
         if oerr, ok := errors.(*net.OpError); ok {
             switch oerr.Err.(type) {
                 case *os.SyscallError:
-                    fmt.Println("connect: connection refused to", host, "on port", port )
+                    PrintString = fmt.Sprintf("%v - connect: connection refused to %v on port %v", endnow, host, port)
                 default:
                     if oerr.Timeout() {
-                        fmt.Println("connect: connection timed out to", host, "on port", port )
+                        PrintString = fmt.Sprintf("%v - connection timed out to %v on port %v", endnow, host, port)
                     } else {
                         if strings.Contains(errors.Error(), "no such host") {
-                            fmt.Println("connect: no such host", host)
+                            PrintString = fmt.Sprintf("%v - connect: no such host %v", endnow, host)
                         } else {
                             panic("Unknown connection error")
                         }
                     }
             }
         }
+        	color.Red(PrintString)
     } else {
-    fmt.Println("connect: connection successful to", host, "on port", port )
+        PrintString = fmt.Sprintf("%v - connect: connection successful to %v on port %v", endnow, host, port)
+        color.Green(PrintString)
     }
 
     if conn != nil {
         conn.Close()
     }
     fmt.Println(" ")
+    fmt.Println("Ble,ble,ble that's all folks ")
 }
